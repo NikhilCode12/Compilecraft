3dc262ee18667c4e112ba8fe46f2cfe848777346
NikhilCode12 <promaxchessace@gmail.com>
2024-01-23 06:11:57 +0530

    add & update: hero added, Navbar button style change

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+
+const portfolioHandler = () => {
+  alert("my portfolio button clicked");
+};
+const startHandler = () => {
+  alert("get started button clicked");
+};
+
+const buttons = [
+  { text: "My Portfolio", handler: portfolioHandler },
+  { text: "Get Started", handler: startHandler },
+];
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex flex-col justify-center items-center mt-12">
+      {/* Heading Tagline */}
+      <section className="flex gap-1 justify-center">
+        <img src={logo} width={32} height={32} alt="logo" />
+        <span className="text-white text-[12px] font-medium my-auto">
+          Transforming Code into Reality
+        </span>
+      </section>
+      {/* Heading */}
+      <div className="flex flex-col gap-3 my-4 text-center">
+        <p className="text-white text-5xl font-semibold">
+          Coding Visions into Reality
+        </p>
+        <p className="text-white text-5xl font-semibold">
+          {` { `}
+          <span className="text-teal-400 italic">Compilecraft</span>
+          {` } `}Where Every Line Counts!
+        </p>
+      </div>
+      {/* Info Para */}
+      <p className="text-gray-400 text-[15px] font-medium text-center w-[55%] my-4">
+        Embrace simplicity without compromising efficiency, as CompileCraft
+        Editor empowers you to craft clean and functional code with ease.
+      </p>
+      <div className="flex gap-6 my-3">
+        {buttons.map((item) => (
+          <button
+            key={item.id}
+            onClick={item.handler}
+            className="btn text-white font-medium rounded-full border-[1px] border-teal-300 px-6 py-2
+            hover:bg-teal-200  hover:border-slate-500 active:border-slate-900 active:bg-teal-500
+             hover:text-[#0b0019] hover:-translate-y-1 hover:transition-all hover:ease-in-out"
+          >
+            {item.text}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -26,7 +26,13 @@ const Navbar = () => {
     <div className="navbar-container m-4 flex justify-between">
       {/* Logo */}
       <div className="logo-container flex gap-2">
-        <img className="logo-img" src={logo} width={64} height={64}></img>
+        <img
+          className="logo-img"
+          src={logo}
+          width={64}
+          height={64}
+          alt="logo"
+        />
         <a
           href="/"
           className="logo-text text-[white] text-[20px] my-auto font-[montserrat] font-semibold"
@@ -55,8 +61,8 @@ const Navbar = () => {
           <button
             key={item.id}
             onClick={item.handler}
-            className="btn text-white font-medium rounded-full border-[1px] border-pink-400 px-6 py-2
-            hover:border-pink-500 active:border-pink-900 hover:translate-y-1 hover:transition-all hover:ease-in"
+            className="btn text-white font-medium rounded-full border-[1px] border-teal-400 px-6 py-2
+            hover:border-teal-500 active:border-teal-900 hover:translate-y-1 hover:transition-all hover:ease-in"
           >
             {item.text}
           </button>
